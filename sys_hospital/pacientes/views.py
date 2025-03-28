d2dc30267c4e98b6772f0c832f920578c9448f7d
BrunoDuaarte <brunofinan2022@gmail.com>
2025-03-28 01:37:13 -0300

    projeto hospital

---
+++ sys_hospital/pacientes/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Paciente
+from .serializers import PacienteSerializer
+
+class PacienteViewSet(viewsets.ModelViewSet):
+    queryset = Paciente.objects.all()
+    serializer_class = PacienteSerializer
+    permission_classes = [permissions.AllowAny]
