a06cbf6bb716642f6d5aef13a45d331bb2bd64df
BrunoDuaarte <brunofinan2022@gmail.com>
2025-04-03 00:41:00 -0300

    insercoes

--- sys_hospital/app/settings.py
+++ sys_hospital/app/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'enderecos',
     'pacientes',
+    'doencas',
+    'enfermeiros',
+    'medicos',
 ]
 
 MIDDLEWARE = [
---
+++ sys_hospital/status_consulta/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StatusConsultaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'status_consulta'
